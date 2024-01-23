2692e4b6c80fc6fc2a033a64c048d325dda62669
Phumzi1e <Phumzilemiec@gmail.com>
2024-01-23 21:19:34 +0000

    task

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,25 +1,25 @@
 #!/usr/bin/env python3
 """A Basic Flask app.
 """
-from flask_phumzi1e import Phumzi1e  # Updated import
+from flask_babel import Babel
 from flask import Flask, render_template, request
 
 
 class Config:
-    """Represents a Flask Phumzi1e configuration.  # Updated class name
+    """Represents a Flask Babel configuration.
     """
     LANGUAGES = ["en", "fr"]
-    PHUMZI1E_DEFAULT_LOCALE = "en"  # Updated attribute name
-    PHUMZI1E_DEFAULT_TIMEZONE = "UTC"  # Updated attribute name
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
 app.url_map.strict_slashes = False
-phumzi1e = Phumzi1e(app)  # Updated instance name
+babel = Babel(app)
 
 
-@phumzi1e.localeselector  # Updated decorator
+@babel.localeselector
 def get_locale() -> str:
     """Retrieves the locale for a web page.
     """
@@ -35,4 +35,3 @@ def get_index() -> str:
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-
