c4f32eafb6996fa9986fa939159c3cb1c5b2802f
Phumzi1e <Phumzilemiec@gmail.com>
2024-01-23 21:17:07 +0000

    task

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 """A Basic Flask app.
 """
-from flask_babel import Phumzi1e
+from flask_babel import Babel
 from flask import Flask, render_template
 
 
 class Config:
-    """Represents a Flask phumzi1e configuration.
+    """Represents a Flask Babel configuration.
     """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
